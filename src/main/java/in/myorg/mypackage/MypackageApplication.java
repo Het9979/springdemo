ffddf3b462c3a037e45b0a0e4c5ace83da6e1a9c
Krishna <krishnashah131103@gmail.com>
2022-07-19 13:59:56 +0530

    first app

---
+++ src/main/java/in/myorg/mypackage/MypackageApplication.java
@@ -0,0 +1,14 @@
+package in.myorg.mypackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MypackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypackageApplication.class, args);
+	}
+
+}
---
+++ src/main/java/in/myorg/mypackage/controller/DemoController.java
@@ -0,0 +1,18 @@
+package in.myorg.mypackage.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class DemoController {
+    @GetMapping("/")
+    public String welcomeMessage(){
+        return "Welcome to the world of spring boot";
+    }
+    @GetMapping("/getDate")
+    public Date getDate(){
+        return new Date();
+    }
+}
